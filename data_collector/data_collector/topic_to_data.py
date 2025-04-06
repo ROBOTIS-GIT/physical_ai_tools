4bbec295f5e77d22b8ee6d41a3d0fc2283fcec89
Seongwoo Kim <kimsw@robotis.com>
2025-04-07 08:27:00 +0900

    removed unnecessary code
    Signed-off-by: Seongwoo Kim <kimsw@robotis.com>

--- data_collector/data_collector/topic_to_data.py
+++ data_collector/data_collector/topic_to_data.py
@@ -22,7 +22,6 @@ class RobotDataSubscriber(Node):
                 'neck_joint1', 'neck_joint2', 'linear_joint'
             ]
         )
-
         self.declare_parameter(
             'joint_order_leader_r_hand', [
                 'right_thumb_1_joint', 'right_thumb_2_joint',
@@ -214,80 +213,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-# import rclpy
-# from rclpy.node import Node
-# from rclpy.time import Time
-
-# from sensor_msgs.msg import JointState
-# from message_filters import Subscriber, ApproximateTimeSynchronizer
-
-# import numpy as np
-# import torch
-# from typing import Optional
-
-
-# class RobotDataSubscriber(Node):
-#     def __init__(self):
-#         super().__init__('robot_data_subscriber')
-        
-#         self.declare_parameter('expected_joint_order_follower', ['joint1', 'joint2', 'joint3', 'joint4', 'gripper_left_joint'])
-#         self.declare_parameter('expected_joint_order_leader', ['joint1', 'joint2', 'joint3', 'joint4', 'gripper_left_joint'])
-#         self.expected_joint_order_follower = self.get_parameter('expected_joint_order_follower').value
-#         self.expected_joint_order_leader = self.get_parameter('expected_joint_order_leader').value
-#         self.latest_observation = None
-#         self.latest_action = None
-
-#         # Create message_filters.Subscriber for each topic
-#         self.obs_sub = Subscriber(self, JointState, '/joint_states')
-#         self.act_sub = Subscriber(self, JointState, '/leader/joint_states')  # TODO: Replace with actual topic like /leader/joint_states
-
-#         # Synchronize the topics with approximate time
-#         self.sync = ApproximateTimeSynchronizer(
-#             [self.obs_sub, self.act_sub],
-#             queue_size=10,
-#             slop=0.05  # Allowable time difference in seconds
-#         )
-#         self.sync.registerCallback(self.synced_callback)
-
-#     def synced_callback(self, obs_msg: JointState, act_msg: JointState):
-#         obs_pos_map = dict(zip(obs_msg.name, obs_msg.position))
-#         act_pos_map = dict(zip(act_msg.name, act_msg.position))
-
-#         ordered_obs = [obs_pos_map[name] for name in self.expected_joint_order_follower]
-#         ordered_act = [act_pos_map[name] for name in self.expected_joint_order_leader]
-            
-#         obs_tensor = torch.tensor(ordered_obs, dtype=torch.float32)
-#         act_tensor = torch.tensor(ordered_act, dtype=torch.float32)
-
-#         # Convert ROS time to float (seconds)
-#         obs_stamp = Time.from_msg(obs_msg.header.stamp).nanoseconds / 1e9
-#         act_stamp = Time.from_msg(act_msg.header.stamp).nanoseconds / 1e9
-
-#         self.latest_observation = {
-#             'observation.state': obs_tensor,
-#             # 'timestamp': obs_stamp,
-#         }
-#         self.latest_action = {
-#             'action': act_tensor,
-#             # 'timestamps': act_stamp,
-#         }
-
-#         self.get_logger().info(f'Received observation: {obs_tensor} @ {obs_stamp:.3f}s')
-#         self.get_logger().info(f'Received action: {act_tensor} @ {act_stamp:.3f}s')
-
-#     def get_latest_data(self) -> Optional[tuple[dict, dict]]:
-#         if self.latest_observation is not None and self.latest_action is not None:
-#             return self.latest_observation, self.latest_action
-#         return None, None
-
-
-# def main(args=None):
-#     rclpy.init(args=args)
-#     node = RobotDataSubscriber()
-#     rclpy.spin(node)
-#     node.destroy_node()
-#     rclpy.shutdown()
-
-# if __name__ == '__main__':
-#     main() 
